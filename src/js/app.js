ced8ab8a6c6b66288be1f655adebb2363d31fce9
gomeseric <gomesericson.a@gmail.com>
2024-06-17 22:58:47 +0000

    test2

--- src/js/app.js
+++ src/js/app.js
@@ -6,9 +6,9 @@ import "../style/index.css";
  * 
     {
         includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
+        background: "https://images.unsplash.com/photo-1521587760476-6c12a4b040da?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bGlicmFyeSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D", // this is the image's url that will be used as a background for the profile cover
         avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
+        socialMediaPosition: "left", // social media bar position (left or right)
         
         twitter: null, // social media usernames
         github: null,
